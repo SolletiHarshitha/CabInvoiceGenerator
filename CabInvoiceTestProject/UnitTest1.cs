f42d435eceebaf5a1758398c3dc16f9439f0c608
solletiharshitha <solletiharshitha@gmail.com>
2021-07-29 01:56:46 +0530

    [Harshitha] Calculate average value

--- CabInvoiceTestProject/UnitTest1.cs
+++ CabInvoiceTestProject/UnitTest1.cs
@@ -29,7 +29,7 @@ namespace CabInvoiceTestProject
             Assert.AreEqual(expected, fare);
         }
         /// <summary>
-        /// Calculating total are for multiple rides
+        /// Calculating total fare for multiple rides
         /// </summary>
         [TestMethod]
         [TestCategory("Multiple Rides")]
@@ -44,6 +44,22 @@ namespace CabInvoiceTestProject
             var res = summary.Equals(expectedSummary);
             //Assert
             Assert.IsNotNull(res);
+        }
+        /// <summary>
+        /// Calculating the average value
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Average")]
+        public void GivenDistanceAndTimeShouldReturnAverage()
+        {
+            ///AAA Methodology
+            //Arrange
+            Ride[] rides = { new Ride(12.0, 5), new Ride(3.5, 1) };
+            //Act
+            InvoiceSummary summary = invoice.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 161.0);
+            //Assert
+            Assert.AreEqual(summary, expectedSummary);
         }
     }
 }
