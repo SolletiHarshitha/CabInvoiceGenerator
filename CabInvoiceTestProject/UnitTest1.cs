cb411dd099533a014f6c1d3d8c379ade0c37eaef
solletiharshitha <solletiharshitha@gmail.com>
2021-07-29 00:18:21 +0530

    [Harshitha] Calculate total fare

---
+++ CabInvoiceTestProject/UnitTest1.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CabInvoiceGenerator;
+namespace CabInvoiceTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        InvoiceGenerator invoice;
+        [TestInitialize]
+        public void SetUp()
+        {
+            invoice = new InvoiceGenerator();
+        }
+
+        [TestMethod]
+        [TestCategory("Total Fare")]
+        public void GivenDistanceAndTimeShouldReturnTotalFare()
+        {
+            ///AAA Methodology
+            //Arrange
+            double distance = 2.0;
+            int time = 5;
+            //Act
+            double fare = invoice.CalculateFare(distance, time);
+            double expected = 25;
+            //Assert
+            Assert.AreEqual(expected, fare);
+        }
+    }
+}
